9113f6061603f8e8a982b9ac3af6eeeab9056420
Jinhyeok <gjwlsgur123@gmail.com>
2022-11-11 12:25:18 +0900

    heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000;
-
+const path = require('path')
 app.use(express.static(path.join(__dirname,"/client/build")))
 app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname,'client/build/index.html'))
+  res.sendFile(path.join(__dirname,'client/build','index.html'))
 })
 
 
