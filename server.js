829717147b350ad61a47e32e47abc013ec91b623
Jinhyeok <gjwlsgur123@gmail.com>
2022-11-11 12:15:46 +0900

    heroku

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname,"/client/build")))
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname,'client/build/index.html'))
+})
+
+
 app.listen(PORT , ()=>{
   console.log(`Connected! : ${PORT}`)
 })
